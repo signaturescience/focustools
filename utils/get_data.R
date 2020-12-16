265de9938f454090c667d0b25adf672b3d831263
VP Nagraj <nagraj@nagraj.net>
2020-12-16 17:34:07 -0500

    adding arg for granularity in get_* data util functions; also adding cumulative counts

--- utils/get_data.R
+++ utils/get_data.R
@@ -1,8 +1,7 @@
-## TODO: add a way to optionally summarize by county or state
 ## TODO: check weekly case count logic (i.e. disentangling cumulative cases) is working
 ## TODO: flesh out cache = TRUE idea (option to use pre-downloaded object)
 
-get_cases <- function(source = "jhu", cache = TRUE) {
+get_cases <- function(source = "jhu", granularity = "national", cache = TRUE) {
 
   if(source == "jhu") {
     ## first read in data
@@ -23,14 +22,51 @@ get_cases <- function(source = "jhu", cache = TRUE) {
       dplyr::group_by(county, fips, state) %>%
       dplyr::arrange(date) %>%
       ## coerce from cumulative to incident cases
-      dplyr::mutate(icases = count - lag(count, default = 0L))
+      ## hold onto count as "ccases" for cumulative cases
+      dplyr::mutate(icases = count - lag(count, default = 0L),
+                    ccases = count)
 
-    ## by usa
-    dat <-
-      dat %>%
-      dplyr::group_by(epiyear, epiweek) %>%
-      dplyr::summarise(icases = sum(icases, na.rm=TRUE), .groups="drop") %>%
-      head(-1)
+    ## by county
+    if (granularity == "county") {
+      dat <-
+        dat %>%
+        ## within each county (fips code), year, week grouping
+        dplyr::group_by(fips,epiyear,epiweek) %>%
+        dplyr::summarise(icases = sum(icases, na.rm = TRUE), .groups = "drop") %>%
+        dplyr::filter(epiweek != lubridate::week(Sys.Date())) %>%
+        dplyr::mutate(date = as.Date(paste(epiyear, epiweek, 1, sep="-"), "%Y-%U-%u")) %>%
+        dplyr::group_by(fips) %>%
+        dplyr::arrange(fips,date) %>%
+        dplyr::mutate(ccases = cumsum(icases)) %>%
+        dplyr::ungroup()
+    } else if(granularity == "state") {
+      dat <-
+        dat %>%
+        ## within each state, year, week grouping
+        dplyr::group_by(state,epiyear,epiweek) %>%
+        ## sum up the number of deaths at that week
+        dplyr::summarise(icases = sum(icases, na.rm = TRUE), .groups = "drop") %>%
+        ## ignore the current week because it will likely be incomplete ...
+        dplyr::filter(epiweek != lubridate::week(Sys.Date())) %>%
+        ## create a variable for date using epiyear and week
+        dplyr::mutate(date = as.Date(paste(epiyear, epiweek, 1, sep="-"), "%Y-%U-%u")) %>%
+        ## now group by statee
+        dplyr::group_by(state) %>%
+        ## arrange by state first then date (ascending)
+        dplyr::arrange(state,date) %>%
+        ## then use the arranged data and cumsum to get at the cumulative deaths at each week/state
+        dplyr::mutate(ccases = cumsum(icases)) %>%
+        dplyr::ungroup() %>%
+        ## NOTE: for now this only keeps state names (not territories)
+        dplyr::filter(state %in% datasets::state.name)
+    } else if (granularity == "national") {
+      ## by usa
+      dat <-
+        dat %>%
+        dplyr::group_by(epiyear, epiweek) %>%
+        dplyr::summarise(icases = sum(icases, na.rm=TRUE), .groups="drop") %>%
+        dplyr::mutate(ccases = cumsum(icases))
+    }
 
   } else if (source == "nyt") {
     dat <-
@@ -38,20 +74,34 @@ get_cases <- function(source = "jhu", cache = TRUE) {
       dplyr::mutate(epiyear=MMWRweek::MMWRweek(date)$MMWRyear, .after=date) %>%
       dplyr::mutate(epiweek=MMWRweek::MMWRweek(date)$MMWRweek, .after=epiyear) %>%
       # dplyr::mutate(week=week(date), .after=epiweek) %>%
-      dplyr::mutate(icases  = cases  - lag(cases, default = 0L))
+      dplyr::mutate(icases  = cases  - lag(cases, default = 0L),
+                    ccases = cases)
 
     ## by usa
-    dat <-
-      dat %>%
-      dplyr::group_by(epiyear, epiweek) %>%
-      dplyr::summarise(icases = sum(icases, na.rm=TRUE), .groups="drop") %>%
-      head(-1)
+    if(granularity == "national") {
+      dat <-
+        dat %>%
+        dplyr::group_by(epiyear, epiweek) %>%
+        dplyr::summarise(icases = sum(icases, na.rm=TRUE), .groups="drop") %>%
+        dplyr::arrange(epiyear,epiweek) %>%
+        dplyr::mutate(ccases = cumsum(icases))
+    } else {
+      stop("for source='nyt' granularity must be 'national' (still working on incorporating 'county' and 'state') ... ")
+    }
 
   }
+
+  ## return the data *except for* the current week (because that may be incomplete)
+  this_week <- paste0(lubridate::year(Sys.Date()), "-", lubridate::week(Sys.Date()))
+
+  dat %>%
+    mutate(ind = paste0(epiyear, "-", epiweek)) %>%
+    filter(ind != this_week) %>%
+    select(-ind)
 }
 
 
-get_deaths <- function(source = "jhu", cache = TRUE) {
+get_deaths <- function(source = "jhu", granularity = "national", cache = TRUE) {
 
   if(source == "jhu") {
     ## first read in data
@@ -72,14 +122,51 @@ get_deaths <- function(source = "jhu", cache = TRUE) {
       dplyr::group_by(county, fips, state) %>%
       dplyr::arrange(date) %>%
       ## coerce from cumulative to incident deaths
-      dplyr::mutate(ideaths = count - lag(count, default = 0L))
+      ## hold onto count as "cdeaths" for cumulative deaths
+      dplyr::mutate(ideaths = count - lag(count, default = 0L),
+                    cdeaths = count)
 
-    ## by usa
-    dat <-
-      dat %>%
-      dplyr::group_by(epiyear, epiweek) %>%
-      dplyr::summarise(ideaths = sum(ideaths, na.rm=TRUE), .groups="drop") %>%
-      head(-1)
+    ## by county
+    if (granularity == "county") {
+      dat <-
+        dat %>%
+        ## within each county (fips code), year, week grouping
+        dplyr::group_by(fips,epiyear,epiweek) %>%
+        dplyr::summarise(ideaths = sum(ideaths, na.rm = TRUE), .groups = "drop") %>%
+        filter(epiweek != lubridate::week(Sys.Date())) %>%
+        mutate(date = as.Date(paste(epiyear, epiweek, 1, sep="-"), "%Y-%U-%u")) %>%
+        group_by(fips) %>%
+        arrange(fips,date) %>%
+        mutate(cdeaths = cumsum(ideaths)) %>%
+        ungroup()
+    } else if(granularity == "state") {
+      dat <-
+        dat %>%
+        ## within each state, year, week grouping
+        dplyr::group_by(state,epiyear,epiweek) %>%
+        ## sum up the number of deaths at that week
+        dplyr::summarise(ideaths = sum(ideaths, na.rm = TRUE), .groups = "drop") %>%
+        ## ignore the current week because it will likely be incomplete ...
+        dplyr::filter(epiweek != lubridate::week(Sys.Date())) %>%
+        ## create a variable for date using epiyear and week
+        dplyr::mutate(date = as.Date(paste(epiyear, epiweek, 1, sep="-"), "%Y-%U-%u")) %>%
+        ## now group by statee
+        dplyr::group_by(state) %>%
+        ## arrange by state first then date (ascending)
+        dplyr::arrange(state,date) %>%
+        ## then use the arranged data and cumsum to get at the cumulative deaths at each week/state
+        dplyr::mutate(cdeaths = cumsum(ideaths)) %>%
+        dplyr::ungroup() %>%
+        ## NOTE: for now this only keeps state names (not territories)
+        dplyr::filter(state %in% datasets::state.name)
+    } else if (granularity == "national") {
+      ## by usa
+      dat <-
+        dat %>%
+        dplyr::group_by(epiyear, epiweek) %>%
+        dplyr::summarise(ideaths = sum(ideaths, na.rm=TRUE), .groups="drop") %>%
+        mutate(cdeaths = cumsum(ideaths))
+    }
 
   } else if (source == "nyt") {
     dat <-
@@ -87,15 +174,35 @@ get_deaths <- function(source = "jhu", cache = TRUE) {
       dplyr::mutate(epiyear=MMWRweek::MMWRweek(date)$MMWRyear, .after=date) %>%
       dplyr::mutate(epiweek=MMWRweek::MMWRweek(date)$MMWRweek, .after=epiyear) %>%
       # dplyr::mutate(week=week(date), .after=epiweek) %>%
-      dplyr::mutate(ideaths  = deaths  - lag(deaths, default = 0L))
+      dplyr::mutate(ideaths  = deaths - lag(deaths, default = 0L),
+                    cdeaths = deaths)
 
     ## by usa
     dat <-
       dat %>%
       dplyr::group_by(epiyear, epiweek) %>%
-      dplyr::summarise(ideaths = sum(ideaths, na.rm=TRUE), .groups="drop") %>%
-      head(-1)
+      dplyr::summarise(ideaths = sum(ideaths, na.rm=TRUE), .groups="drop")
+
+    ## by usa
+    if(granularity == "national") {
+      dat <-
+        dat %>%
+        dplyr::group_by(epiyear, epiweek) %>%
+        dplyr::summarise(ideaths = sum(ideaths, na.rm=TRUE), .groups="drop") %>%
+        dplyr::arrange(epiyear,epiweek) %>%
+        dplyr::mutate(cdeaths = cumsum(ideaths))
+    } else {
+      stop("for source='nyt' granularity must be 'national' (still working on incorporating 'county' and 'state') ... ")
+    }
 
   }
+
+  ## return the data *except for* the current week (because that may be incomplete)
+  this_week <- paste0(lubridate::year(Sys.Date()), "-", lubridate::week(Sys.Date()))
+
+  dat %>%
+    mutate(ind = paste0(epiyear, "-", epiweek)) %>%
+    filter(ind != this_week) %>%
+    select(-ind)
 }
 
