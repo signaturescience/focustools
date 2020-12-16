bfdf6931ebe39be529b6d1bff703a5be4ac8b3db
VP Nagraj <nagraj@nagraj.net>
2020-12-16 15:05:17 -0500

    updating mars code to use get_* data utils; added mars deaths scratch script

--- scratch/mars-deaths.R
+++ scratch/mars-deaths.R
@@ -1,4 +1,4 @@
-## initial exploration with MARS model and lagged predictor (cases)
+## initial exploration with MARS model and lagged predictor (deaths)
 ## https://doi.org/10.2307/2290499
 ## https://cran.r-project.org/web/packages/earth/index.html
 ## TODO: look into using tidymodels to implement this model (MARS is one of the parsnip engines ...)
@@ -12,10 +12,10 @@ library(lubridate)
 source("utils/get_data.R")
 
 usa <-
-  get_cases(source = "jhu") %>%
+  get_deaths(source = "jhu") %>%
   rename(week = epiweek) %>%
   filter(week != lubridate::week(Sys.Date())) %>%
-  mutate(lag_cases = lag(icases, default = 0L))
+  mutate(lag_deaths = lag(ideaths, default = 0L))
 
 ## evaluate how well the model fits
 ## create separate training / test sets
@@ -31,7 +31,7 @@ usa_test <-
 
 ## "default" params for earth()
 usa_mars_fit <- earth(
-  icases ~ lag_cases,
+  ideaths ~ lag_deaths,
   data = usa_train,
   nfold = 10,
   ncross = 10,
@@ -45,6 +45,7 @@ predict(usa_mars_fit, newdata = usa_test, interval = "pint") %>%
   ## prediction interval tibble drops the week
   ## make sure to add that column back in
   mutate(week = usa_test$week) %>%
+  mutate(epiyear = usa_test$epiyear) %>%
   ## join the data together by "week" column
   ## NOTE: the right-hand side is the training and testing stacked on top
   ## this allows us to see the predicted vs observed for the last 4 weeks
@@ -54,12 +55,12 @@ predict(usa_mars_fit, newdata = usa_test, interval = "pint") %>%
   ## prediction interval for forecasts
   geom_ribbon(aes(x = date, y = fit, ymin = lwr, ymax = upr), fill = "lightpink", alpha = 0.5) +
   ## line for observed
-  geom_line(aes(date, icases), lwd = 1.25) +
+  geom_line(aes(date, ideaths), lwd = 1.25) +
   ## line for predicted
   geom_line(aes(date, fit), col = "firebrick", lwd = 1.25) +
   theme_minimal() +
   scale_y_continuous(labels=scales::number_format()) +
-  labs(x = "Date", y = "Weekly COVID-19 Incidence")
+  labs(x = "Date", y = "Weekly COVID-19 Deaths (National)")
 
 
 #####################################################
@@ -69,7 +70,7 @@ usa_train <- usa
 
 ## refit the model
 usa_mars_fit <- earth(
-  icases ~ lag_cases,
+  ideaths ~ lag_deaths,
   data = usa_train,
   nfold = 10,
   ncross = 10,
@@ -87,7 +88,7 @@ res <- list()
 for(i in 1:horizon) {
 
   ## if first week in horizon then use the last of observed data
-  if(i == 1) oneweek_horizon <- tail(usa_train$icases, 1)
+  if(i == 1) oneweek_horizon <- tail(usa_train$ideaths, 1)
 
   ## create the input tibble to be passed to predict()
   ## use the last week number from training plus the horizon index (e.g. 2 weeks ahead of last week = 45 => 47)
@@ -102,8 +103,7 @@ for(i in 1:horizon) {
     whead <- whead - 52
     epiyear <- epiyear + 1
   }
-
-  usa_future <- tibble(week = whead, lag_cases = oneweek_horizon)
+  usa_future <- tibble(week = whead, lag_deaths = oneweek_horizon)
 
   ## predict
   tmp_res <- predict(usa_mars_fit, newdata = usa_future, interval = "pint")
@@ -130,18 +130,22 @@ do.call("rbind", res) %>%
   ## prediction interval for forecasts
   geom_ribbon(aes(x = date, y = fit, ymin = lwr, ymax = upr), fill = "lightpink", alpha = 0.5) +
   ## line for observed
-  geom_line(aes(date, icases), lwd = 1.25) +
+  geom_line(aes(date, ideaths), lwd = 1.25) +
   ## line for predicted
   geom_line(aes(date, fit), col = "firebrick", lwd = 1.25) +
   theme_minimal() +
   scale_y_continuous(labels=scales::number_format()) +
-  labs(x = "Date", y = "Weekly COVID-19 Incidence")
+  labs(x = "Date", y = "Weekly COVID-19 Deaths (National)")
 
 
 ## try to get quantiles for 1w ahead
 ## first week in horizon then use the last of observed data
-oneweek_horizon <- tail(usa_train$icases, 1)
+oneweek_horizon <- tail(usa_train$cases, 1)
 
+## create the input tibble to be passed to predict()
+## use the last week number from training plus the horizon index (e.g. 2 weeks ahead of last week = 45 => 47)
+## lag_cases is set at either the last value in cases (if first horizon index)
+## or the fitted value ("fit") from the prediction for the previous horizon week (see below)
 ## create the input tibble to be passed to predict()
 ## use the last week number from training plus the horizon index (e.g. 2 weeks ahead of last week = 45 => 47)
 ## lag_cases is set at either the last value in cases (if first horizon index)
@@ -156,7 +160,7 @@ if(whead > 52) {
   epiyear <- epiyear + 1
 }
 
-usa_future <- tibble(week = whead, lag_cases = oneweek_horizon)
+usa_future <- tibble(week = whead, lag_deaths = oneweek_horizon)
 
 ## predict
 ## create standard error (TODO: check with CHL and SDT about SE for prediction interval formula here ...)
